4fa9201e7e4a62fb3d792ed0d9604ffd46c2bc4d
Erick Jesús Guzmán Ascencio <71535124+ErickJGAscencio@users.noreply.github.com>
2023-08-05 18:18:32 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,46 @@
+
+window.onload = function() {
+    var texto = document.getElementById('txt-1');
+    texto.classList.add('aparecer');
+    texto.style.opacity = 1;
+    texto.style.transform = 'translateY(0px)';
+    texto.style.zIndex = 1;
+
+    
+    var texto = document.getElementById('txt-2');
+    texto.classList.add('aparecer');
+    texto.style.opacity = 1;
+    texto.style.transform = 'translateY(0px)';
+}
+
+
+var video = document.getElementById("videoPlayer");
+var seekBar = document.getElementById("seekBar");
+var titulo = document.getElementById("titulo");
+
+var videoName;
+
+// function playPause() {
+//     if (video.paused)
+//         video.play();
+//     else
+//         video.pause();
+// }
+
+function setNameVideo(name, titulo) {
+    videoName = name;
+    LoadVideo(titulo);
+}
+
+function LoadVideo(tituloT) {
+    var newRoot = "Videos/" + videoName;
+    video.setAttribute('src', newRoot);
+    titulo.innerText = tituloT;
+    video.load();
+    video.play();
+}
+
+
+
+
+
